a05884fad496f8fddfdb4df05822c187448f25fe
Jason Taylor <jasongt@outlook.com>
2020-11-05 11:08:35 +1000

    GettingStarted-Initial commit

---
+++ CaWorkshop/CaWorkshop.WebUI/Data/ApplicationDbContextSeeder.cs
@@ -0,0 +1,31 @@
+﻿using CaWorkshop.WebUI.Models;
+using System.Linq;
+
+namespace CaWorkshop.WebUI.Data
+{
+    public static class ApplicationDbContextSeeder
+    {
+        public static void Seed(ApplicationDbContext context)
+        {
+            if (context.TodoLists.Any())
+            {
+                return;
+            }
+
+            var list = new TodoList
+            {
+                Title = "Todo List",
+                Items =
+                {
+                    new TodoItem { Title = "Make a todo list" },
+                    new TodoItem { Title = "Check off the first item" },
+                    new TodoItem { Title = "Realise you've already done two things on the list!"},
+                    new TodoItem { Title = "Reward yourself with a nice, long nap" },
+                }
+            };
+
+            context.TodoLists.Add(list);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ CaWorkshop/CaWorkshop.WebUI/Program.cs
@@ -0,0 +1,52 @@
+using CaWorkshop.WebUI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CaWorkshop.WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services
+                        .GetRequiredService<ApplicationDbContext>();
+
+                    context.Database.Migrate();
+
+                    ApplicationDbContextSeeder.Seed(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services
+                        .GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while " +
+                        "migrating or initializing the database.");
+
+                    throw;
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
